3cf9d27f181296fd5c05d313ba693dcd4d7ae828
Subi suku <97378615+7thRuler@users.noreply.github.com>
2022-12-20 15:41:58 +0530

    Add files via upload

---
+++ p9bit.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+int n1,n2,n3;
+printf("enter the size of the setA: ");
+scanf("%d",&n1);
+
+
+int a[n1],;
+for(i=0;i=n1;i++)
+{
+	printf("enter the elements of set A:");
+	scanf("%d"&a[i]);
+}
+
+
+printf("enter the size of set B:");
+scanf("%d",&n2);
+
+
+int [n2];
+for(i=0;i=n1;i++)
+{
+	printf("enter the elements of set B:");
+	scanf("%d"&b[i]);
+}
+
+
+void main()
+{
+    int x,a;
+    while(1)
+    {
+     printf("set menu");
+     printf("\n1.union\n2.intr\n3.diff\n4.exit\n");
+     printf("enter your chice(1/2/3/4): ");
+     scanf("\n%d",&x);
+     switch(x)
+     {
+     case 1:
+     	union1();
+     	break;
+     case 4:
+     	exit(0);
+     }
+}
+void union1()
+{
+	
+}
